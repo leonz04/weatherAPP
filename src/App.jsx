669b2f5757137212911ce68be881cee4e0e1db3f
andresfleon <andresfleon@gmail.com>
2024-01-02 18:43:53 -0500

    V0.0

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
+import './index.css'
+import './components/WeatherCard.css'
+import WeatherCard from './components/WeatherCard'
+
+function App() {
+
+  const [coords, setCoords]=useState()
+  const [wheater, setwheater] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const [temp, setTemp] = useState()
+
+  
+
+  const getDataLocation= pos=>{
+    const obj ={
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    }
+    setCoords(obj)
+    
+    console.log(pos)
+       
+  }  
+
+  useEffect(()=>{
+    setIsLoading(true)
+    navigator.geolocation.getCurrentPosition(getDataLocation)
+
+    
+  },[])
+
+  useEffect(()=>{
+
+    if (coords){
+      const API_KEY='7dac2439f4fc00cc375e1bab983133d1'
+      const {lat, lon}=coords
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`      
+
+      axios.get(url)
+      .then(res =>{
+        setwheater(res.data)
+        const celsius= (res.data.main.temp-273.15).toFixed(1)
+        const obj ={
+          celsius: celsius,
+          fahrentheit: ((res.data.main.temp-273.15)*(9/5)+32).toFixed(1)
+        }
+        setTemp(obj);
+      })
+      .catch(err=>console.log(err))
+      .finally(()=>setIsLoading(false))
+
+    }
+
+  },[coords])
+
+
+
+  return (
+   <div className='app'>
+    {
+      isLoading
+      ? <h2>Loading...</h2>
+      :(<WeatherCard
+      weather={wheater}
+      temp ={temp}/>)
+       
+    }
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+const WeatherCard = ({weather , temp, }) => {
+
+
+    const [isCelsius, setIsCelsius] = useState(true)
+    
+
+
+    const handleChangeTemp=()=>{
+
+        setIsCelsius(state=>!state)
+        
+    }
+ 
+
+  return (
+    <article className='weather'>
+        <h1 className='weather__title'>Weather App</h1>
+        <h2 className='weather__subtitle'>{weather?.name}, {weather?.sys.country}</h2>
+        <section className='weather__body'>
+            <header className='weather__img__container'>
+                <img className='weather__icon' src={weather && `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png` } alt='imagen del clima'/>
+            </header>
+            <article className='weather__info'>
+                <h3 className='weather__info__title'>"{weather?.weather[0].description}"</h3>
+                <ul className='weather__list'>
+                    <li className='weather__item'>
+                        <span className='weather__label'>Wind Speed</span>
+                        <span className='weather__value'> {weather?.wind.speed} m/s</span>
+                    </li>
+                    <li className='weather__item'>
+                        <span className='weather__label'>Pressure</span>
+                        <span className='weather__value'> {weather?.main.pressure} hPa</span>
+                    </li>
+                    <li className='weather__item'>
+                        <span className='weather__label'>Clouds</span>
+                        <span className='weather__value'> {weather?.wind.speed} %</span>
+                    </li>
+                </ul>
+            </article>
+        </section>
+        <footer className='weather__footer'>
+            <h2 className='weather__temp'>{isCelsius
+                ?`${temp?.celsius} °C`
+                : `${temp?.fahrentheit} °F`}
+            </h2>
+            <button className='weather__btn' onClick={handleChangeTemp}>Change Temperature</button>
+            <form >
+                <input placeholder='Ingresar Ciudad'/>
+                <button type='submit'>Consultar</button>
+            </form>
+        </footer>
+    </article>
+  )
+}
+
+export default WeatherCard
